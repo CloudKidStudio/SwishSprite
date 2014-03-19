85265c6d5fa41a708647e154c57fdb85a850a04d
Matt Karl <matt@mattkarl.com>
2014-03-19 16:33:02 -0400

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "cloudkid.AudioUtils",
+        "cloudkid.EventDispatcher",
+        "cloudkid.PageVisibility",
+        "cloudkid.SwishSprite"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
